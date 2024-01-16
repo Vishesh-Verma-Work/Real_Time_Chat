64a2fafc57e5baa600fcb37cba45c66790bbd713
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2024-01-16 09:34:23 -0800

    Added connect, disconnect, msz ui and react setup, and socket code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import {io} from "socket.io-client";
+import {Container} from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const handelSubmit = (e)=> {
+      e.preventDefault();
+  };
+
+  useEffect(()=> {
+    socket.on("connect", ()=> {
+     console.log("Connected by useEffect by ID : ", socket.id);;
+    })
+
+    socket.on("welcome-msz", (msz)=> {
+      console.log(msz);
+    })
+
+
+    return ()=> {
+      socket.disconnect();
+    };
+
+  }, []);
+
+  const [count, setCount] = useState(0)
+  const socket  = io("http://localhost:3000");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Container maxWidth="sm"> 
+     <h3>Welcome to our chat app</h3>
+     <form onSubmit={handelSubmit}>
+      <input type="text" />
+      <button type='submit'>Send Chat</button>
+     </form>
+
+     </Container>
     </>
   )
 }
---
+++ server/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {Server} from "socket.io";
+import {createServer} from "http";
+
+const  app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors : {
+        origin : "http://localhost:5173",
+        methods : ["GET", "POST"],
+        credentials : true
+    }
+}); 
+
+io.on("connection", (socket)=> {
+    console.log(`Connted to the server, Socket ID : ${socket.id}`);
+    socket.emit("welcome-msz", `Welcome to the server ID : ${socket.id}`);
+
+    socket.on("disconnect", ()=> {
+        console.log(`User ID ${socket.id} disconnected`);
+    })
+
+
+});
+
+const port = 3000;
+
+server.listen(port, ()=>{
+    console.log(`Server is running on port number : ${port}`);
+});
+
+app.get("/", (req,res)=> {
+    res.send("Hello world");
+});
